ef709ecb87a5d9229ea55f0bf5ac9be5322082ad
element2562 <element25668@gmail.com>
2018-09-26 21:11:37 -0500

    New solution for larger files

--- ConsoleApp2/ConsoleApp2/Program.cs
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Linq;
+using System.IO;
 namespace ConsoleApp2
 {
     class Program
@@ -21,7 +22,35 @@ namespace ConsoleApp2
             longWord = splitText.Where(y => y.Length == maxLetters).FirstOrDefault();
             
             Console.WriteLine($"{maxLetters}, {longWord}");
-            
+            ////////////////////////////////////////////////////////////////////////////////////////////
+            string path = @"C:\Users\Jacob\workspace\dad's exercises\2\textfile.txt";
+            int chunkSize = 1024;
+            using (var file = File.OpenRead(path))
+            {
+                int offset = 0;
+                int bytesRead;
+                var Buffer = new Byte[chunkSize];
+                while((bytesRead = file.Read(Buffer, offset, Buffer.Length)) > 0)
+                {
+                    offset += bytesRead;
+                    string newText = Buffer.ToString();
+                    var newSplitText = newText.Split(" ");
+                    string newLongWord = "";
+                    int newMaxLetters;
+                    newMaxLetters = newSplitText.Max(y => y.Length);
+                    //for(int i = 0; i < splitText.Length; i++)
+                    //{
+                    //    if(splitText[i].Length == maxLetters)
+                    //    {
+                    //        longWord = splitText[i];
+                    //    }
+                    //}
+                    newLongWord = newSplitText.Where(y => y.Length == maxLetters).FirstOrDefault();
+
+                    Console.WriteLine($"{newMaxLetters}, {newLongWord}");
+                }
+
+            }
 
         }
     }
